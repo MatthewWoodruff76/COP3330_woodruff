ea511b962936fb45f7c4de980a8b17f92e81301b
Matthew Woodruff <matthewwoodruff76@knights.ucf.edu>
2020-10-10 09:24:06 -0400

    Applied Inheritance

--- woodruf_p3/src/Shape.java
+++ woodruf_p3/src/Shape.java
@@ -1,4 +1,11 @@
 public abstract class Shape {
+    abstract void getName();
+    abstract void getArea();
 
 }
+abstract class Shape2D extends Shape{
 
+}
+abstract class Shape3D extends Shape{
+
+}
