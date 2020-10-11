6b9e0a8aa976ad3ce6267e3dbfbb94136838affb
Matthew Woodruff <matthewwoodruff76@knights.ucf.edu>
2020-10-10 22:46:33 -0400

    Real Start

--- woodruf_p3/src/Shape.java
+++ woodruf_p3/src/Shape.java
@@ -1,11 +1,23 @@
-public abstract class Shape {
-    abstract void getName();
-    abstract void getArea();
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+//Progenitor Class
+abstract class Shape {
+    protected static int side;
+    public abstract String getName();
+    public abstract Integer getArea();
 }
-abstract class Shape2D extends Shape{
 
-}
-abstract class Shape3D extends Shape{
 
+//Subclass
+class Square extends Shape {
+    private int mySide;
+    public Square(int side) {
+        mySide = side;
+    }
+    public String getName() {
+        return "square";
+    }
+    public Integer getArea(){
+        return mySide*mySide;
+    }
 }
---
+++ woodruf_p3/src/Shape2D.java
@@ -0,0 +1,18 @@
+/*
+public abstract class Shape2D {
+
+
+    public abstract double getArea();
+}
+
+class Square extends Shape2D {
+    public Square(double i) {
+        super();
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+}
+*/
