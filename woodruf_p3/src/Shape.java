28aac42622951acf0c27ab33dd6a205d2ba73df7
Matthew Woodruff <matthewwoodruff76@knights.ucf.edu>
2020-10-11 17:14:57 -0400

    Using Interfaces

--- woodruf_p3/src/Shape.java
+++ woodruf_p3/src/Shape.java
@@ -1,23 +1,150 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-//Progenitor Class
-abstract class Shape {
-    protected static int side;
-    public abstract String getName();
-    public abstract Integer getArea();
+interface Shape {
+    String getName();
+    double getArea();
+    double getVolume();
+}
+interface Shape2D {
+    String getName();
+    double getArea();
+    double getVolume();
+}
+interface Shape3D {
+    String getName();
+    double getArea();
+    double getVolume();
 }
 
-
-//Subclass
-class Square extends Shape {
-    private int mySide;
-    public Square(int side) {
-        mySide = side;
+class Square implements Shape, Shape2D {
+    public static double side = 0;
+    public Square(double input) {
+        side = input;
     }
+    @Override
     public String getName() {
         return "square";
     }
-    public Integer getArea(){
-        return mySide*mySide;
+    @Override
+    public double getArea() {
+        return side*side;
+    }
+    @Override
+    public double getVolume() { //empty
+        return 0;
+    }
+}
+
+class Triangle implements Shape, Shape2D {
+    public static double length = 0;
+    public static double width = 0;
+    public Triangle(double input1, double input2) {
+        length = input1;
+        width = input2;
+    }
+    @Override
+    public String getName() {
+        return "triangle";
+    }
+    @Override
+    public double getArea() { //Returns area
+        return 0.5*width*length;
+    }
+    @Override
+    public double getVolume() { //Empty
+        return 0;
+    }
+}
+
+class Circle implements Shape, Shape2D {
+    public static double radius = 0;
+    public Circle(double input) {
+        radius = input;
+    }
+    @Override
+    public String getName() {
+        return "circle";
+    }
+    @Override
+    public double getArea() { //Returns the area
+        return Math.PI*radius*radius;
+    }
+    @Override
+    public double getVolume() { //Empty
+        return 0;
+    }
+}
+
+class Cube implements Shape, Shape2D, Shape3D {
+    public static double side = 0;
+    public Cube(double input) {
+        side = input;
+    }
+    @Override
+    public String getName() {
+        return "cube";
+    }
+    @Override
+    public double getArea() { //Returns the area
+        return 6*side*side;
+    }
+    @Override
+    public double getVolume() { //Returns the volume
+        return side*side*side;
+    }
+}
+
+class Pyramid implements Shape, Shape2D, Shape3D {
+    public static double height = 0;
+    public static double width = 0;
+    public static double length = 0;
+    public Pyramid(double input1, double input2, double input3) {
+        length = input1;
+        width = input2;
+        height = input3;
+    }
+    @Override
+    public String getName() {
+        return "pyramid";
+    }
+    @Override
+    public double getArea() { //Returns the area
+        double sidesArea = sideArea1(length, width, height)+sideArea2(length, width, height);
+        return base(width,length)+ sidesArea;
+    }
+
+    @Override
+    public double getVolume() { //Returns the volume
+        return height*width*length/3.0;
+    }
+    private double Pythag(double height, double width){ //Performs the Pythagorean equation
+        return Math.sqrt(height*height+width*width);
+    }
+    private double base(double width, double length){
+        return width*length;
+    }
+    private double sideArea1(double length, double width, double height){
+        return width*Math.sqrt(height*height+0.25*length*length);
+    }
+    private double sideArea2(double length, double width, double height){
+        return length*Math.sqrt(height*height+0.25*width*width);
+    }
+
+}
+
+class Sphere implements Shape, Shape2D, Shape3D {
+    public static double radius = 0;
+    public Sphere(double input) {
+        radius = input;
+    }
+    @Override
+    public String getName() {
+        return "sphere";
+    }
+    @Override
+    public double getArea() { //Returns the area
+        return 4.0*Math.PI*radius*radius;
+    }
+    @Override
+    public double getVolume() { //Returns the volume
+        return 4.0/3.0*Math.PI*radius*radius*radius;
     }
 }
