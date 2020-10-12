b3e1c8a2bf16e9918948c32f44c88d2d698413b8
Matthew Woodruff <matthewwoodruff76@knights.ucf.edu>
2020-10-12 08:14:49 -0400

    Final Draft

--- woodruf_p3/src/Shape.java
+++ woodruf_p3/src/Shape.java
@@ -1,128 +1,127 @@
 abstract class Shape {
-    abstract String getName();
-    abstract double getArea();
+    abstract String getName();              //Returns a pre-scripted shape name.
+    abstract double getArea();              //Returns the area, calculated in the return statement.
 }
-abstract class Shape2D extends Shape{ //Empty per rubic
+abstract class Shape2D extends Shape{
 }
 abstract class Shape3D extends Shape{
-    abstract double getVolume();
+    abstract double getVolume();            //Returns the volume, calculated in the return statement.
 }
 
-class Square extends Shape2D {
-    public static double side = 0;
-    public Square(double input) {
+class Square extends Shape2D {              //The shape subclasses extend their respective dimensions.
+    private static double side = 0;
+    protected Square(double input) {        //The shape methods assign the input to corresponding variables.
         side = input;
     }
     @Override
-    public String getName() {
+    protected String getName() {
         return "square";
     }
     @Override
-    public double getArea() {
+    protected double getArea() {
         return side*side;
     }
 }
 
 class Triangle extends Shape2D {
-    public static double length = 0;
-    public static double width = 0;
-    public Triangle(double input1, double input2) {
+    private static double length = 0;
+    private static double width = 0;
+    protected Triangle(double input1, double input2) {
         length = input1;
         width = input2;
     }
     @Override
-    public String getName() {
+    protected String getName() {
         return "triangle";
     }
     @Override
-    public double getArea() { //Returns area
+    protected double getArea() {
         return 0.5*width*length;
     }
 }
 
 class Circle extends Shape2D {
-    public static double radius = 0;
-    public Circle(double input) {
+    private static double radius = 0;
+    protected Circle(double input) {
         radius = input;
     }
     @Override
-    public String getName() {
+    protected String getName() {
         return "circle";
     }
     @Override
-    public double getArea() { //Returns the area
+    protected double getArea() {
         return Math.PI*radius*radius;
     }
 }
 
 class Cube extends Shape3D {
-    public static double side = 0;
-    public Cube(double input) {
+    private static double side = 0;
+    protected Cube(double input) {
         side = input;
     }
     @Override
-    public String getName() {
+    protected String getName() {
         return "cube";
     }
     @Override
-    public double getArea() { //Returns the area
+    protected double getArea() {
         return 6*side*side;
     }
     @Override
-    public double getVolume() { //Returns the volume
+    protected double getVolume() {
         return side*side*side;
     }
 }
 
 class Pyramid extends Shape3D {
-    public static double height = 0;
-    public static double width = 0;
-    public static double length = 0;
-    public Pyramid(double input1, double input2, double input3) {
+    private static double height = 0;
+    private static double width = 0;
+    private static double length = 0;
+    protected Pyramid(double input1, double input2, double input3) {
         length = input1;
         width = input2;
         height = input3;
     }
     @Override
-    public String getName() {
+    protected String getName() {
         return "pyramid";
     }
     @Override
-    public double getArea() { //Returns the area
-        double sidesArea = sideArea1(length, width, height)+sideArea2(length, width, height);
-        return base(width,length)+ sidesArea;
+    protected double getArea() {
+        return baseArea(width,length)+ sideArea1(length, width, height)+sideArea2(length, width, height);
     }
 
     @Override
-    public double getVolume() { //Returns the volume
+    protected double getVolume() {
         return height*width*length/3.0;
     }
-    private double base(double width, double length){
+    private double baseArea(double width, double length){                   //Finds the area of the pyramid base
         return width*length;
     }
-    private double sideArea1(double length, double width, double height){
+    private double sideArea1(double length, double width, double height){   //Finds the first term of the side area
         return width*Math.sqrt(height*height+0.25*length*length);
     }
-    private double sideArea2(double length, double width, double height){
+    private double sideArea2(double length, double width, double height){   //Finds the second term of the side area
         return length*Math.sqrt(height*height+0.25*width*width);
     }
 }
 
 class Sphere extends Shape3D {
-    public static double radius = 0;
-    public Sphere(double input) {
+    private static double radius = 0;
+    protected Sphere(double input) {
         radius = input;
     }
     @Override
-    public String getName() {
+    protected String getName() {
         return "sphere";
     }
     @Override
-    public double getArea() { //Returns the area
+    protected double getArea() {
         return 4.0*Math.PI*radius*radius;
     }
     @Override
-    public double getVolume() { //Returns the volume
+    protected double getVolume() {
         return 4.0/3.0*Math.PI*radius*radius*radius;
     }
 }
