9ddd6cf01d30c3fc8c352a54f537a4a1229fb57c
Matthew Woodruff <matthewwoodruff76@knights.ucf.edu>
2020-11-08 22:47:02 -0500

    Rough draft complete, moving on to testing.

--- woodruff_p4/src/App.java
+++ woodruff_p4/src/App.java
@@ -1,9 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
+
 
 public class App {
     public static void main(String[] args) {
@@ -14,22 +18,47 @@ public class App {
             MainSelection = MenuHandler(3);
             if(MainSelection == 1){
                 System.out.println("A new task list has been created.\n\n");
-                int OperationSelection = 1776;
-                while(OperationSelection != 8) {
-                    PrintOperationMenu();
-                    OperationSelection = MenuHandler(8);
-                    TaskAction(OperationSelection, Archive.Docket);
-                }
+                OperationMenu(Archive.Docket);
             }
             if(MainSelection == 2){
-                System.out.println("You entered 2!");
+                System.out.println("Output file name (no extension required): ");
+                Scanner in = new Scanner(System.in);
+                String FileNameIn = in.nextLine() + ".txt";
+                String placeholder, titleIn = "Error", descriptionIn = "Error", due_dateIn = "Error";
+                boolean statusIn;
+                try {
+                    BufferedReader input = new BufferedReader(new FileReader(FileNameIn));
+                    int tally = 1;
+                    while((placeholder = input.readLine()) != null) {
+                        if((tally + 1) % 4 == 0) titleIn = placeholder;
+                        if((tally + 2) % 4 == 0) descriptionIn = placeholder;
+                        if((tally + 3) % 4 == 0) due_dateIn = placeholder;
+                        if(tally % 4 == 0){
+                            statusIn = Boolean.parseBoolean(placeholder);
+                            TaskItem Task = new TaskItem(titleIn, descriptionIn, due_dateIn, statusIn);
+                            Archive.Docket.add(Task);
+                        }
+                        tally++;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("The list was loaded successfully.\n");
+                OperationMenu(Archive.Docket);
             }
         }
     }
-    private static void TaskAction(int selection, ArrayList<TaskItem> Docket) {
-        if(selection == 1) {
-            PrintAllTasks(Docket);
+    public static String FileNameOut;
+    private static void     OperationMenu(ArrayList<TaskItem> Docket){
+        int OperationSelection = 1776;
+        while(OperationSelection != 8) {
+            PrintOperationMenu();
+            OperationSelection = MenuHandler(8);
+            TaskAction(OperationSelection, Docket);
         }
+    }
+    private static void     TaskAction(int selection, ArrayList<TaskItem> Docket) {
+        if(selection == 1) PrintAllTasks(Docket);
         if(selection == 2) {
             String titleIn =        GetTitle();
             String descriptionIn =  GetDescription();
@@ -70,10 +99,47 @@ public class App {
             }
         }
         if(selection == 7) {
-            //You have chosen to save the current list
+                System.out.print("\n\nOutput file name (no extension required): ");
+                CreateFile();
+                SaveTaskList(Docket);
         }
     }
-    private static int[] GenerateCompletionKey(ArrayList<TaskItem> Docket, boolean complete) {
+    private static void     SaveTaskList(ArrayList<TaskItem> Docket) {
+            try {
+                FileWriter SavePoint = new FileWriter(FileNameOut);
+                SavePoint.write(AmassListInfo(Docket));
+                SavePoint.close();
+                System.out.println("\nTasks saved to " + FileNameOut + "\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    }
+    private static String   AmassListInfo(ArrayList<TaskItem> Docket) {
+        String ListInfo = "";                                           //Empty opening statement.
+        for(int index = 0; index < Docket.size(); index++){
+            ListInfo += AmassTaskInfo(Docket.get(index));
+        }
+        return ListInfo;
+    }
+    private static String   AmassTaskInfo(TaskItem TaskItem) {
+        return  TaskItem.title + "\n" + TaskItem.description + "\n"
+                + TaskItem.due_date + "\n" + TaskItem.complete + "\n" ;
+    }
+    private static void     CreateFile() {
+        try {
+            String FileNameOut = GetFileName() + ".txt";
+            File myObj = new File(FileNameOut);
+            if (myObj.createNewFile()) {
+                System.out.println("The task list was saved under this name: " + myObj.getName());
+            } else {
+                System.out.println("A file with that name already exists there...");
+            }
+        } catch (IOException e) {
+            System.out.println("Something went wrong...");
+            e.printStackTrace();
+        }
+    }
+    private static int[]    GenerateCompletionKey(ArrayList<TaskItem> Docket, boolean complete) {
         int AmountValid = 0;
         for (int index = 0; index < Docket.size(); index++) {
             if (Docket.get(index).complete == complete) AmountValid++;
@@ -88,45 +154,31 @@ public class App {
         }
         return key;
     }
-    private static void PrintTasksByCompletion(ArrayList<TaskItem> Docket, boolean complete) {
-        if(complete == false) System.out.println("Current Incomplete Tasks\n------------------------\n\n");
-        else System.out.println("Current Complete Tasks\n----------------------\n\n");
-        int position = 0;
-        for(int index = 0; index < Docket.size();index ++) {
-            if(Docket.get(index).complete == complete) {
-                System.out.print((position + 1) + ") [" + Docket.get(index).due_date + "] "
-                        + Docket.get(index).title + ": " + Docket.get(index).description + "\n");
-                position++;
-            }
-        }
+    private static String   GetFileName(){
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        FileHandler(input);
+        return input;
     }
-    private static void PrintAllTasks(ArrayList<TaskItem> Docket) {
-        System.out.println("Current Tasks\n" +
-                "-------------\n\n");
-        for(int index = 0; index < Docket.size();index ++) {
-            System.out.print((index + 1) + ") [" + Docket.get(index).due_date + "] "
-                    + Docket.get(index).title + ": " + Docket.get(index).description + "\n");
-        }
-    }
-    private static String GetTitle() {
+    private static String   GetTitle() {
         System.out.print("\nTask title: ");
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
         return TitleHandler(input);
     }
-    private static String GetDescription() {
+    private static String   GetDescription() {
         System.out.print("Task description: ");
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
         return DescriptionHandler(input);
     }
-    private static String GetDue_Date() {
+    private static String   GetDue_Date() {
         System.out.print("Task due date (YYYY-MM-DD): ");
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
         return Due_DateHandler(input);
     }
-    private static void PrintOperationMenu() {
+    private static void     PrintOperationMenu() {
         System.out.println("\nList Operation Menu\n" +
                 "---------\n\n" +
                 "1) view the list\n" +
@@ -138,38 +190,44 @@ public class App {
                 "7) save the current list\n" +
                 "8) quit to the main menu\n\n");
     }
-    private static void PrintMainMenu() {
+    private static void     PrintMainMenu() {
         System.out.println("Main Menu\n---------\n\n" +
                 "1) create a new list\n" +
                 "2) load an existing list\n" +
                 "3) quit\n\n");
     }
-    private static int MenuHandler(int length) {
-        Scanner in = new Scanner(System.in);
-        int selection;
-        try{
-            selection = in.nextInt();
+    private static void     PrintTasksByCompletion(ArrayList<TaskItem> Docket, boolean complete) {
+        if(!complete) System.out.println("Current Incomplete Tasks\n------------------------\n\n");
+        else System.out.println("Current Complete Tasks\n----------------------\n\n");
+        int position = 0;
+        for(int index = 0; index < Docket.size();index ++) {
+            if(Docket.get(index).complete == complete) {
+                System.out.print((position + 1) + ") [" + Docket.get(index).due_date + "] "
+                        + Docket.get(index).title + ": " + Docket.get(index).description + "\n");
+                position++;
+            }
         }
-        catch (Exception e){
-            System.out.println("\nThat wasn't even a number...  Try again.\n");
-            selection = MenuHandler(length);
-        }
-        if (selection > length || selection <= 0){
-            System.out.println("\nThat was not a listed option. Try again.\n");
-            selection = MenuHandler(length);
-        }
-        return selection;
     }
-    private static String TitleHandler(String input) {
-        if(input.length()==0) {
-            return GetTitle();
+    private static void     PrintAllTasks(ArrayList<TaskItem> Docket) {
+        System.out.println("Current Tasks\n" +
+                "-------------\n\n");
+        for(int index = 0; index < Docket.size();index ++) {
+            System.out.print((index + 1) + ") [" + Docket.get(index).due_date + "] "
+                    + Docket.get(index).title + ": " + Docket.get(index).description + "\n");
         }
-        return input;   //Verifies length.
     }
-    private static String DescriptionHandler(String input) {
+    private static void     FileHandler(String input){  //Checks for blatant violations in naming.
+        String[] criminals = { "/", "\n", "\r", "\t", "\0", "\f", "`", "?", "*", "<", ">", "|", ":", String.valueOf((char)34)};
+        for(int index = 0; index < criminals.length; index++) {
+            if (input.contains(criminals[index])) {
+                GetFileName();
+            }
+        }
+    }
+    private static String   DescriptionHandler(String input) {
         return input;   //Does not have error handling.
     }
-    private static String Due_DateHandler(String input) {
+    private static String   Due_DateHandler(String input) {
         int[] validDays =   {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
         String date = formatter.format(new Date());
@@ -178,7 +236,7 @@ public class App {
             return GetDue_Date();
         }     //Handles length errors before reaching sensitive code.
 
-        int year = 0, yearNow = 0, month = 0, monthNow = 0, day = 0, dayNow = 0, valid = 1, test, testNow;
+        int year, yearNow, month, monthNow, day, dayNow, valid = 1;
 
         year        = StringToInt(0,4,input);
         month       = StringToInt(5,7,input);
@@ -201,11 +259,34 @@ public class App {
 
         return GetDue_Date();
     }
-    private static int FormulaHandler(String input) {
+    private static String   TitleHandler(String input) {
+        if(input.length()==0) {
+            System.out.println("\nAt least one character is required.\n\n");
+            return GetTitle();
+        }
+        return input;   //Verifies length.
+    }
+    private static int      MenuHandler(int length) {
+        Scanner in = new Scanner(System.in);
+        int selection;
+        try{
+            selection = in.nextInt();
+        }
+        catch (Exception e){
+            System.out.println("\nThat wasn't even a number...  Try again.\n");
+            selection = MenuHandler(length);
+        }
+        if (selection > length || selection <= 0){
+            System.out.println("\nThat was not a listed option. Try again.\n");
+            selection = MenuHandler(length);
+        }
+        return selection;
+    }
+    private static int      FormulaHandler(String input) {
         if(input.charAt(4) != 45 || input.charAt(7) != 45) return 2; //Checks for dashes.
         return 1;
     }
-    private static int StringToIntHandler(int start, int end, String input) {
+    private static int      StringToIntHandler(int start, int end, String input) {
         int valid = 1;
         for(int index = start; index < end; index ++) {
             int test = input.charAt(index) - 48;
@@ -213,7 +294,7 @@ public class App {
         }
         return valid;
     }
-    private static int StringToInt(int start, int end, String input) {
+    private static int      StringToInt(int start, int end, String input) {
         int output = 0, digit, index;
         for(index = start; index < end; index ++){
             digit = input.charAt(index)-48;
@@ -221,13 +302,13 @@ public class App {
         }
         return output;
     }
-    private static int CalendarHandler(int year, int month, int day, int[] validDays) {
+    private static int      CalendarHandler(int year, int month, int day, int[] validDays) {
         if(year  % 4 == 0) validDays[1] = 29;                       //February will be destroyed
         if(month > 12 || month < 1) return 5;
         if(day > validDays[month - 1]) return 5;
         return 1;
     }
-    private static int PastHandler(int year, int yearNow, int month, int monthNow, int day, int dayNow) { //Checks for due dates in the past.
+    private static int      PastHandler(int year, int yearNow, int month, int monthNow, int day, int dayNow) { //Checks for due dates in the past.
         if(year >= yearNow) {
             if(year == yearNow){
                 if(month>=monthNow) {
@@ -242,5 +323,11 @@ public class App {
             }
         } else return 3;
         return 1;
+    }
+
+
+    @Test
+    public void MenuHandlerValid(){
+            assertEquals(1,1);
     }
 }
